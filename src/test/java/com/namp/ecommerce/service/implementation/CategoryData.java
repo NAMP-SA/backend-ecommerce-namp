9afe3f420fabf53f7f9fbfd6d3f7f5c2a079be3b
RoberSalera <robertosalera15@gmail.com>
2025-04-24 17:28:28 -0300

    Testing Category

--- src/test/java/com/namp/ecommerce/service/implementation/CategoryData.java
+++ src/test/java/com/namp/ecommerce/service/implementation/CategoryData.java
@@ -5,17 +5,56 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.namp.ecommerce.dto.CategoryDTO;
+import com.namp.ecommerce.dto.CategoryWithSubcategoriesDTO;
+import com.namp.ecommerce.dto.SubcategoryDTO;
 import com.namp.ecommerce.model.Category;
 
 public class CategoryData {
 
-    public final static List<Category> CATEGORIAS = Arrays.asList(
-            new Category(1L, "Bebidas con alcohol", "Bebidas para mayores de edad", new ArrayList<>()),
-            new Category(2L, "Bebidas sin alcohol", "Bebidas para cualuiqer persona", new ArrayList<>()));
+        public final static List<Category> CATEGORIAS = Arrays.asList(
+                        new Category(1L, "Bebidas con alcohol", "Bebidas con alcohol", new ArrayList<>()),
+                        new Category(2L, "Bebidas sin alcohol", "Bebidas sin alcohol", new ArrayList<>()));
 
+        public final static List<CategoryDTO> CATEGORIASDTO = Arrays.asList(
+                        new CategoryDTO(1L, "Bebidas con alcohol", "Bebidas para mayores de edad"),
+                        new CategoryDTO(2L, "Bebidas sin alcohol", "Bebidas para cualuiqer persona"));
 
-    public final static List<CategoryDTO> CATEGORIASDTO = Arrays.asList(
-        new CategoryDTO(1L, "Bebidas con alcohol", "Bebidas para mayores de edad"),
-        new CategoryDTO(2L, "Bebidas sin alcohol", "Bebidas para cualuiqer persona"));
+        public final static CategoryDTO CATEGORIADTO = new CategoryDTO(1L, "Bebidas alcoholicas",
+                        "Bebidas con alcohol");
+
+        public final static Category CATEGORIA = new Category(1L, "BEBIDAS ALCOHOLICAS",
+                        "Bebidas con alcohol", new ArrayList<>());
+
+        public final static CategoryDTO CATEGORIADTO_ACTUALIZADA = new CategoryDTO(1L,
+                        "BEBIDAS ALCOHOLICAS ACTUALIZADA",
+                        "Bebidas con alcohol");
+
+        public final static Category CATEGORIA_ACTUALIZADA = new Category(1L, "BEBIDAS ALCOHOLICAS ACTUALIZADA",
+                        "Bebidas con alcohol", new ArrayList<>());
+
+        // Get con subcategorias
+
+        public final static CategoryDTO BEBIDAS_ALCOHOL_DTO = new CategoryDTO(1L, "Bebidas con alcohol",
+                        "Bebidas con alcohol");
+        public final static CategoryDTO BEBIDAS_NO_ALCOHOL_DTO = new CategoryDTO(2L, "Bebidas sin alcohol",
+                        "Bebidas sin alcohol");
+
+        public final static List<SubcategoryDTO> SUBCATEGORIAS_ALCOHOL_DTO = Arrays.asList(
+                        new SubcategoryDTO(1L, "Vodka", "Bebida blanca", BEBIDAS_ALCOHOL_DTO),
+                        new SubcategoryDTO(2L, "Cinzano", "Aperitivo", BEBIDAS_ALCOHOL_DTO));
+
+        public final static List<SubcategoryDTO> SUBCATEGORIAS_NO_ALCOHOL_DTO = Arrays.asList(
+                        new SubcategoryDTO(3L, "Gaseosa", "Bebida con gas", BEBIDAS_NO_ALCOHOL_DTO),
+                        new SubcategoryDTO(4L, "Jugos", "Jugos de frutas", BEBIDAS_NO_ALCOHOL_DTO));
+
+        public final static CategoryWithSubcategoriesDTO CATEGORIA_SUBCATEGORIAS = new CategoryWithSubcategoriesDTO(1L,
+                        "Bebidas alcoholicas", "Bebidas alcoholicas",
+                        SUBCATEGORIAS_ALCOHOL_DTO, null);
+
+        public final static List<CategoryWithSubcategoriesDTO> CATEGORIAS_SUBCATEGORIAS = Arrays.asList(
+                        new CategoryWithSubcategoriesDTO(1L, "Bebidas alcoholicas", "Bebidas alcoholicas",
+                                        SUBCATEGORIAS_ALCOHOL_DTO, null),
+                        new CategoryWithSubcategoriesDTO(2L, "Bebidas sin alcohol", "Bebidas sin alcohol",
+                                        SUBCATEGORIAS_NO_ALCOHOL_DTO, null));
 
 }
--- src/test/java/com/namp/ecommerce/service/implementation/CategoryTest.java
+++ src/test/java/com/namp/ecommerce/service/implementation/CategoryTest.java
@@ -1,8 +1,10 @@
 package com.namp.ecommerce.service.implementation;
 
-
 import com.namp.ecommerce.dto.CategoryDTO;
+import com.namp.ecommerce.dto.CategoryWithSubcategoriesDTO;
 import com.namp.ecommerce.mapper.MapperCategory;
+import com.namp.ecommerce.model.Category;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +15,6 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
-
 
 import com.namp.ecommerce.repository.ICategoryDAO;
 
@@ -41,7 +42,8 @@ public class CategoryTest {
     @Test
     void findCategoryById() {
         when(repository.findByIdCategory(1L)).thenReturn(CategoryData.CATEGORIAS.get(0));
-        when(mapperCategory.convertCategoryToDto(CategoryData.CATEGORIAS.get(0))).thenReturn(CategoryData.CATEGORIASDTO.get(0));
+        when(mapperCategory.convertCategoryToDto(CategoryData.CATEGORIAS.get(0)))
+                .thenReturn(CategoryData.CATEGORIASDTO.get(0));
 
         CategoryDTO category = service.findById(1L);
 
@@ -54,8 +56,9 @@ public class CategoryTest {
     void findCategories() {
         when(repository.findAll()).thenReturn(CategoryData.CATEGORIAS);
 
-        for (int i = 0; i < (CategoryData.CATEGORIAS.size()); i++){
-            when(mapperCategory.convertCategoryToDto(CategoryData.CATEGORIAS.get(i))).thenReturn(CategoryData.CATEGORIASDTO.get(i));
+        for (int i = 0; i < (CategoryData.CATEGORIAS.size()); i++) {
+            when(mapperCategory.convertCategoryToDto(CategoryData.CATEGORIAS.get(i)))
+                    .thenReturn(CategoryData.CATEGORIASDTO.get(i));
         }
 
         List<CategoryDTO> categories = service.getCategories();
@@ -66,5 +69,68 @@ public class CategoryTest {
         assertEquals(categories.get(1), CategoryData.CATEGORIASDTO.get(1));
     }
 
-}
+    @Test
+    void findCategorYWithSubcategories() {
+        when(repository.findByIdCategory(1L)).thenReturn(CategoryData.CATEGORIAS.get(0));
+        when(mapperCategory.convertCategoryIdWithSubcategoryToDto(CategoryData.CATEGORIAS.get(0)))
+                .thenReturn(CategoryData.CATEGORIA_SUBCATEGORIAS);
 
+        CategoryWithSubcategoriesDTO categoryWithSubcategoriesDTO = service.getCategoriesIdWithSubcategories(1L);
+
+        assertNotNull(categoryWithSubcategoriesDTO);
+        assertEquals(CategoryData.CATEGORIA_SUBCATEGORIAS, categoryWithSubcategoriesDTO);
+        assertEquals(CategoryData.CATEGORIA_SUBCATEGORIAS.getSubcategories(),
+                categoryWithSubcategoriesDTO.getSubcategories());
+
+    }
+
+    @Test
+    void findCategoriesWithSubcategories() {
+        when(repository.findAll()).thenReturn(CategoryData.CATEGORIAS);
+        for (int i = 0; i < (CategoryData.CATEGORIAS.size()); i++) {
+            when(mapperCategory.convertCategoryWithSubcategoryToDto(CategoryData.CATEGORIAS.get(i)))
+                    .thenReturn(CategoryData.CATEGORIAS_SUBCATEGORIAS.get(i));
+        }
+
+        List<CategoryWithSubcategoriesDTO> categoriesWithSubcategoriesDTO = service.getCategoriesWithSubcategories();
+
+        assertNotNull(categoriesWithSubcategoriesDTO);
+        assertEquals(CategoryData.CATEGORIAS_SUBCATEGORIAS.size(), categoriesWithSubcategoriesDTO.size());
+        assertEquals(CategoryData.CATEGORIAS_SUBCATEGORIAS.get(0), categoriesWithSubcategoriesDTO.get(0));
+        assertEquals(CategoryData.CATEGORIAS_SUBCATEGORIAS.get(1), categoriesWithSubcategoriesDTO.get(1));
+
+    }
+
+    @Test
+    void saveCategory() {
+        CategoryDTO newCategoryDTO = CategoryData.CATEGORIADTO;
+        when(repository.save(any())).thenReturn(CategoryData.CATEGORIA);
+        when(mapperCategory.convertCategoryToDto(CategoryData.CATEGORIA))
+                .thenReturn(CategoryData.CATEGORIADTO);
+
+        CategoryDTO categoryDTO = service.save(newCategoryDTO);
+        assertNotNull(categoryDTO.getIdCategory());
+        assertEquals(1L, categoryDTO.getIdCategory());
+        assertEquals("BEBIDAS ALCOHOLICAS", categoryDTO.getName());
+    }
+
+    @Test
+    void updateCategory() {
+        CategoryDTO newCategoryDTO = CategoryData.CATEGORIADTO;
+        Category existing_category = CategoryData.CATEGORIA;
+
+        when(repository.save(any())).thenReturn(CategoryData.CATEGORIA_ACTUALIZADA);
+        when(repository.findByIdCategory(existing_category.getIdCategory())).thenReturn(CategoryData.CATEGORIA);
+
+        when(mapperCategory.convertCategoryToDto(CategoryData.CATEGORIA_ACTUALIZADA))
+                .thenReturn(CategoryData.CATEGORIADTO_ACTUALIZADA);
+
+        CategoryDTO categoryDTO = service.update(newCategoryDTO, existing_category);
+
+        assertNotNull(categoryDTO);
+        assertEquals(CategoryData.CATEGORIA_ACTUALIZADA.getIdCategory(), categoryDTO.getIdCategory());
+        assertEquals(CategoryData.CATEGORIA_ACTUALIZADA.getName(), categoryDTO.getName());
+
+    }
+
+}
