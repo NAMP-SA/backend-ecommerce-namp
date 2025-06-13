955d1a4ec494dfe85ba869ca5beb82be45ef2866
gonzabaldassi <gonzalobaldassi02@gmail.com>
2025-06-13 16:23:20 -0300

    Fix: WebConfig Eliminacion de la logica vieja de las imagenes

--- src/main/java/com/namp/ecommerce/WebConfig.java
+++ src/main/java/com/namp/ecommerce/WebConfig.java
@@ -8,16 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${image.upload.dir}")
-    private String uploadDir;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
-        // Configuracion del acceso a la carpeta de imagenes
-        registry.addResourceHandler("/images/**").addResourceLocations("file:"+uploadDir+"/");
-    }
-    
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite todas las rutas de la API
