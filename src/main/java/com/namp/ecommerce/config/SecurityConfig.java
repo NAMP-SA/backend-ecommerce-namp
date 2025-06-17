b1c8accb2c07bb65896cc240004f3bc4b9b85acc
gonzabaldassi <gonzalobaldassi02@gmail.com>
2025-06-17 16:38:46 -0300

    Fix: WebConfig Se agrego la url del frontend para permitir los cors

--- src/main/java/com/namp/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/namp/ecommerce/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
             .csrf(AbstractHttpConfigurer::disable)
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .cors(cors -> cors.configurationSource(this.corsConfigurationSource()))
             .authorizeHttpRequests(authRequest ->
                 authRequest
                 //Public endpoint
